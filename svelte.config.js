01cb997e9fe9f3bc8c90ce4008ac8a183f9a9e4a
Kent Travis Donovan <kenttravisdonovan@gmail.com>
2023-12-06 11:04:21 +0700

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	}
-// };
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
+};
 
-// export default config;
+export default config;
 // import adapter from '@sveltejs/adapter-dynamic';
 // was "@sveltejs/adapter-auto"
 
@@ -34,16 +34,16 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 // 	preprocess: [vitePreprocess({})]
 // };
 
-import adapter from '@sveltejs/adapter-node';
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter(),
-    prerender: {
-      enabled: false,
-      entries: ['*'],
-    },
-    // ...other options...
-  },
-};
-export default config;
+// import adapter from '@sveltejs/adapter-node';
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   kit: {
+//     adapter: adapter(),
+//     prerender: {
+//       enabled: false,
+//       entries: ['*'],
+//     },
+//     // ...other options...
+//   },
+// };
+// export default config;
