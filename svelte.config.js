28281b62329aac4a3221fd16a305a5b692ca9eb5
Kent Travis Donovan <kenttravisdonovan@gmail.com>
2023-12-06 11:11:28 +0700

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,38 +1,38 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
-
-export default config;
-// import adapter from '@sveltejs/adapter-dynamic';
-// was "@sveltejs/adapter-auto"
-
-// const dev = 'production' === 'development';
-
-// /** @type {import(""@sveltejs/kit").Config} */
+// /** @type {import('@sveltejs/kit').Config} */
 // const config = {
 // 	kit: {
-// 		adapter: adapter({
-// 			pages: 'docs',
-// 			assets: 'docs'
-// 		}),
-// 		// paths: {
-// 		// 	// change below to your repo name
-// 		// 	base: dev ? '' : '/svelteFrontEnd'
-// 		// }
-// 		// hydrate the <div id="svelte"> element in src/app.html
-// 	},
-
-// 	preprocess: [vitePreprocess({})]
+// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	}
 // };
+
+// export default config;
+import adapter from '@sveltejs/adapter-static';
+// was "@sveltejs/adapter-auto"
+
+const dev = 'production' === 'development';
+
+/** @type {import(""@sveltejs/kit").Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		paths: {
+			// change below to your repo name
+			base: dev ? '' : '/svelteFrontEnd'
+		}
+		// hydrate the <div id="svelte"> element in src/app.html
+	},
+
+	preprocess: [vitePreprocess({})]
+};
 
 // import adapter from '@sveltejs/adapter-node';
 // /** @type {import('@sveltejs/kit').Config} */
@@ -46,4 +46,4 @@ export default config;
 //     // ...other options...
 //   },
 // };
-// export default config;
+export default config;
