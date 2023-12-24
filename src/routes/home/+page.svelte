7a04ace496ed906e55103c5a9818ac8f8de1678c
Ken <ken3lliot@gmail.com>
2023-12-24 17:32:12 +0700

    update home navbar

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -159,36 +159,60 @@
 </script>
 
 <div class="flex space-x-4 text-sm justify-end h-screen mt-3">
-	<div class="justify-end w-1/4">
+	<div class="navbar justify-end w-1/4">
 		<div>
 			<button
 				class="my-4 hover:text-white hover:bg-[#0891b2] text-center p-3 px-5 rounded inline-block cursor-pointer"
-				>Home</button
+				>
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+					<path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
+				  </svg>
+				  Home
+				</button
 			>
 		</div>
 		<div>
 			<button
 				class="my-4 hover:text-white hover:bg-[#0891b2] text-center p-3 px-5 rounded inline-block cursor-pointer"
-				>Explore</button
+				>
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
+					<path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472M3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z"/>
+				  </svg>
+				Explore</button
 			>
 		</div>
 		<div>
 			<button
 				class="my-4 hover:text-white hover:bg-[#0891b2] text-center p-3 px-5 rounded inline-block cursor-pointer"
-				>Bookmark</button
+				>
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
+					<path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
+				  </svg>  
+				Bookmark
+				</button
 			>
 		</div>
 		<div>
-			<button
+			<button 
 				class="my-4 hover:text-white hover:bg-[#0891b2] text-center p-3 px-5 rounded inline-block cursor-pointer"
-				>Profile</button
+				>
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+					<path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+				  </svg>
+				  Profile
+				</button
 			>
 		</div>
 		<div>
 			<button
 				on:click={() => deleteCookie('uid')}
 				class="my-4 hover:text-white hover:bg-[#0891b2] text-center p-3 px-5 rounded inline-block cursor-pointer"
-				>Logout</button
+				>
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+					<path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+					<path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
+				  </svg>
+				Logout</button
 			>
 		</div>
 	</div>
@@ -297,4 +321,23 @@
 	:global(body) {
 		overflow: hidden;
 	}
+	.navbar{
+		background-color: rgb(48, 169, 244);
+		
+	}
+
+	.navbar button{
+		
+		justify-content: center;
+		align-items: center;
+		display: flex;
+		font-size: larger;
+		font-weight: bolder;
+	}
+
+	svg{
+		margin-right: 20px;
+	}
+
+	
 </style>
